ecc4e9fefc204d3891daf8c8990420eb0ba30577
coartem <coartem2@proton.me>
2023-05-11 18:27:39 -0300

    sidebar configurada

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,5 +1,25 @@
 import { Container } from './styles'
 
+import { ReactComponent as BurgerIcon } from '../../assets/burger.svg'
+
+import menuImg from '../../assets/menu.svg'
+
 export function Sidebar () {
-  return <Container>Sidebar</Container>
+  return (
+  <Container>
+    <button type='button'>
+        <img src={menuImg} alt='Abrir e fechar menu' />
+    </button>
+    <nav>
+      <ul>
+        <li>
+          <a href='#'>
+            <BurgerIcon />
+            <span>Hambúrgueres</span>
+          </a>
+        </li>
+      </ul>
+    </nav>
+  </Container>
+  )
 }
