9e2f94d146e0f22ea6ca1b4ee28999bd5123eac2
coartem <coartem2@proton.me>
2023-05-12 17:02:51 -0300

    sidebar pc

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,21 +1,50 @@
+import { useState } from 'react'
+
 import { Container } from './styles'
 
 import { ReactComponent as BurgerIcon } from '../../assets/burger.svg'
+import { ReactComponent as PizzaIcon } from '../../assets/pizza.svg'
+import { ReactComponent as SodaPopIcon } from '../../assets/soda.svg'
+import { ReactComponent as IceCreamIcon } from '../../assets/ice-cream.svg'
 
 import menuImg from '../../assets/menu.svg'
 
 export function Sidebar () {
+  const [menuOpen, setMenuOpen] = useState(false)
+
+  const handleToggleMenu = () => {
+    setMenuOpen(!menuOpen)
+  }
+
   return (
-  <Container>
-    <button type='button'>
+  <Container isMenuOpen={menuOpen}>
+    <button type='button' onClick={handleToggleMenu}>
         <img src={menuImg} alt='Abrir e fechar menu' />
     </button>
     <nav>
       <ul>
         <li>
-          <a href='#'>
+          <a href='#' className='active' >
             <BurgerIcon />
             <span>Hambúrgueres</span>
+          </a>
+        </li>
+        <li>
+          <a href='#'>
+            <PizzaIcon />
+            <span>Pizzas</span>
+          </a>
+        </li>
+        <li>
+          <a href='#'>
+            <SodaPopIcon />
+            <span>Bebidas</span>
+          </a>
+        </li>
+        <li>
+          <a href='#'>
+            <IceCreamIcon />
+            <span>Sobremesas</span>
           </a>
         </li>
       </ul>
