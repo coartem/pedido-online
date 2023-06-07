f4195965bbbe07bd8699a9d5bc8eba048c99cff9
coartem <coartem2@proton.me>
2023-06-07 20:29:57 -0300

    page payment start

--- src/pages/MyCart/index.tsx
+++ src/pages/MyCart/index.tsx
@@ -1,6 +1,9 @@
 import { Container } from './styles'
+
 import { Head } from '../../components/Head'
+
 import { OrderHeader } from '../../components/OrderHeader'
+
 import { Table } from './Table'
 
 export default function Main() {
---
+++ src/pages/Payment/index.tsx
@@ -0,0 +1,12 @@
+import { Head } from "../../components/Head";
+import { OrderHeader } from "../../components/OrderHeader";
+
+
+export default function Payment() {
+  return (
+    <Container>
+    <Head title ='Carrinho' />
+    <OrderHeader />
+  </Container>
+  )
+}
