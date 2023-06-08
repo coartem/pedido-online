a66433c0b0172ac2b90df4863baa6b616cd8892b
coartem <coartem2@proton.me>
2023-06-07 21:53:06 -0300

    payment page

--- src/pages/Payment/index.tsx
+++ src/pages/Payment/index.tsx
@@ -1,5 +1,8 @@
 import { Head } from "../../components/Head";
+
 import { OrderHeader } from "../../components/OrderHeader";
+
+import { Container, Inner, Form } from "./styles";
 
 
 export default function Payment() {
@@ -7,6 +10,35 @@ export default function Payment() {
     <Container>
     <Head title ='Carrinho' />
     <OrderHeader />
+    <Inner>
+      <Form>
+        <h4>Informações pessoais</h4>
+
+        <div className="field">
+          <label htmlFor="full-name">Nome e sobrenome</label>
+          <input type="text" id="full-name" name="full-name" autoComplete="name" />
+        </div>
+
+        <div className="grouped">
+
+          <div className="field">
+            <label htmlFor="email">E-mail</label>
+            <input type="email" id="email" name="email" autoComplete="email" />
+          </div>
+
+          <div className="field">
+            <label htmlFor="mobile">Celular</label>
+            <input type="tel" id="mobile" name="mobile" autoComplete="phone" />
+          </div>
+
+          <div className="field">
+            <label htmlFor="document">CPF / CNPJ</label>
+            <input type="text" id="document" name="document" />
+          </div>
+
+        </div>
+      </Form>
+    </Inner>
   </Container>
   )
 }
--- src/pages/Payment/styles.ts
+++ src/pages/Payment/styles.ts
@@ -6,3 +6,40 @@ export const Container = styled.div`
   padding: 2rem;
   margin: 0 auto;
 `
+
+export const Inner = styled.main`
+  background: ${({ theme }) => theme.colors.black};
+  padding: 2rem 2.5rem;
+  border-radius: 8px;
+`
+export const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+
+    h4 {
+      margin: 2.5rem 0 2rem;
+
+      &:first-child {
+        margin-top: 0;
+      }
+    }
+
+    .grouped {
+      flex-grow: 1;
+      display: flex;
+      flex-direction: row;
+      gap: 1rem;
+      width: 100%;
+    }
+
+    .field {
+      flex-grow: 1;
+      margin-bottom: 1rem;
+    }
+
+    label {
+      display: block;
+      color: ${({ theme }) => theme.colors.white};
+      margin-bottom: 0.5rem;
+    }
+`
