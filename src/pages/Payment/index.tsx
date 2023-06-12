49dd5c8520da100c374ba8d4a3e931340977b7f8
coartem <coartem2@proton.me>
2023-06-12 17:43:37 -0300

    imask

--- src/pages/Payment/index.tsx
+++ src/pages/Payment/index.tsx
@@ -1,8 +1,7 @@
-import { useForm, SubmitHandler } from 'react-hook-form'
+
 import { yupResolver } from '@hookform/resolvers/yup'
 import * as yup from 'yup'
 import { IMaskInput } from 'react-imask'
-
 import { Head } from '../../components/Head'
 import { PayOrder } from '../../components/OrderCloseAction/PayOrder'
 import { OrderHeader } from '../../components/OrderHeader'
@@ -22,6 +21,7 @@ const schema = yup
 
 export default function Payment() {
   const {
+    control,
     register,
     handleSubmit,
     formState: { errors },
@@ -41,20 +41,40 @@ export default function Payment() {
 
           <div className='field'>
             <label htmlFor='fullName'>Nome e sobrenome</label>
-            <input type='text' id='fullName'  autoComplete='name' {...register('fullName')}/>
+
+            <Controller name='fullName' control={control} render={({ field}) => (
+              <input type='text' id='fullName' autoComplete='name' {...field} />
+            )}
+            />
             { errors.fullName && <p className='error'>{errors.fullName.message}</p>}
           </div>
 
           <div className='grouped'>
             <div className='field'>
               <label htmlFor='email'>E-mail</label>
-              <input type='email'  id='email' autoComplete='email' {...register('email')}/>
+
+            <Controller name='email' control={control} render={({ field}) => (
+              <input type='text' id='email' autoComplete='email' {...field} />
+            )}
+            />
+
               { errors.email && <p className='error'>{errors.email.message}</p>}
             </div>
 
             <div className='field'>
               <label htmlFor='mobile'>Celular</label>
-              <input type='tel' id='mobile' autoComplete='phone' {...register('mobile')}/>
+              <Controller
+              name='mobile' control={control} render={({ field}) => (
+                <IMaskInput
+                  type= 'tel'
+                  id= 'mobile'
+                  autoComplete= 'phone'
+                  mask={'(00) 90000-0000'}
+                  {...field}
+                />
+              )}
+            />
+
               { errors.mobile && <p className='error'>{errors.mobile.message}</p>}
             </div>
 
