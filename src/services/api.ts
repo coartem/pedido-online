af6fef359e7d3ca6e14675e736810bac5763df3e
coartem <coartem2@proton.me>
2023-07-05 19:08:09 -0300

    snack

--- src/services/api.ts
+++ src/services/api.ts
@@ -6,9 +6,9 @@ const api = axios.create({
   baseURL: process.env.REACT_APP_API_BASE_URL,
 })
 
-export const getBurgers = () => api.get<SnackData[]>('/burgers')
-export const getPizzas = () => api.get<SnackData[]>('/pizzas')
-export const getDrinks = () => api.get<SnackData[]>('/drinks')
-export const getIceCreams = () => api.get<SnackData[]>('/ice-creams')
+export const getBurgers = () => api.get<SnackData[]>('snacks?snack=burger')
+export const getPizzas = () => api.get<SnackData[]>('/snacks?snack=pizza')
+export const getDrinks = () => api.get<SnackData[]>('snacks?snack=drinks')
+export const getIceCreams = () => api.get<SnackData[]>('snacks?snack=ice-creams')
 
 export default api
